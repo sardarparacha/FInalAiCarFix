f619d3ea0b04e027e8c7c466a036094e50410eb0
sardarparacha <58203458+sardarparacha@users.noreply.github.com>
2024-09-13 09:08:55 +0000

    Update SubscriptionFeature.js

--- app/Components/SubscriptionFeature.js
+++ app/Components/SubscriptionFeature.js
@@ -7,7 +7,7 @@ const SubscriptionFeature = () => {
           <div className="relative w-full h-0" style={{ paddingBottom: '56.25%' }}>
             <iframe
               className="absolute top-0 left-0 w-full h-full"
-              src="https://www.youtube.com/embed/MQP_3OUCqCw?si=KIPMtwgmokWObdCw" // Replace with actual video ID
+              src="i will add later"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
